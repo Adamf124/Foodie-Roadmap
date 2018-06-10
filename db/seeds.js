8388868d13b2561a9c688c07aed49a9ed523a5e7
Adamf124 <adam.ferguson124@gmail.com>
2018-06-10 11:16:54 -0400

    create test user data

--- db/seeds.js
+++ db/seeds.js
@@ -11,3 +11,15 @@ mongoose.connect('mongodb://localhost/Foodie-Roadmap')
   .catch((err) => {
     console.log('ERROR', err)
   })
+// <--- Remove old user Data --->
+user.remove()
+  .then(() => {
+
+    // <--- create new test user data --->
+    const user1 = new user({
+      name: 'Xavier',
+      address: '123 this st., Atlanta, GA ',
+      email_address: 'xa@thismail.com',
+      userID: number
+    })
+  })
