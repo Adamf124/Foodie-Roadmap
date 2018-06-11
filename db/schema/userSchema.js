ce5056d327bf9ef9df118550fee0b3cf71720267
Adamf124 <adam.ferguson124@gmail.com>
2018-06-11 10:23:01 -0400

    add username to user schema

--- db/schema/userSchema.js
+++ db/schema/userSchema.js
@@ -2,6 +2,7 @@ const Schema = require('mongoose').Schema
 
 const userSchema = new Schema({
     name: String,
+    username: String,
     address: String,
     email_address: {
         type: String,
