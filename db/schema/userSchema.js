2a91047eef20431c63e8d1e8c2c539b0eb77abd1
Adamf124 <adam.ferguson124@gmail.com>
2018-06-10 22:24:48 -0400

    create user schema

--- db/schema/userSchema.js
+++ db/schema/userSchema.js
@@ -1,2 +1,13 @@
 const Schema = require('mongoose').Schema
 
+const userSchema = new Schema({
+    name: String,
+    address: String,
+    email_address: {
+        type: String,
+        required: true
+    },
+    userID: Number
+})
+
+module.exports = userSchema
