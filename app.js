cc0b1aad4b30fa12215692f61936b033c1e3e6d1
Adamf124 <adam.ferguson124@gmail.com>
2018-06-11 11:40:57 -0400

    change naming convention

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/userController');
 
 const mongoose = require('mongoose'); 
 mongoose.connect(process.env.MONGODB_URI); 
