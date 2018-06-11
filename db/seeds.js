5270a1191f79d07b02011901a26e689a3479573c
Adamf124 <adam.ferguson124@gmail.com>
2018-06-11 14:49:08 -0400

    updated seeds

--- db/seeds.js
+++ db/seeds.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const mongoose = require('mongoose');
-const Schema = require("./schema.js");
+const user = require("../models/Users");
 
 /// Connect to Database
 mongoose.connect('mongodb://localhost/Foodie-Roadmap')
@@ -18,17 +18,19 @@ mongoose.connect('mongodb://localhost/Foodie-Roadmap')
     // <--- create new test user data --->
     const user1 = new user({
       name: 'Xavier',
+      username: 'thecoolest1',
       address: '123 this st., Atlanta, GA ',
       email_address: 'xa@thismail.com',
       userID: 1
     })
     const user2 = new user({
       name: 'Sean Connery',
+      username: 'theReal007',
       address: '123 this st., Atlanta, GA ',
       email_address: 'sc@thismail.com',
       userID: 2
     })
-    const users = [ user1, user2, user3, user4 ]
+    const users = [ user1, user2, ]
       // <--- save test data --->
       return user.insertMany(users)
     .then(() => {
