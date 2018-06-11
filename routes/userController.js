66ffc1e1aa62e33bb39f62a71784768bf16b9912
Adamf124 <adam.ferguson124@gmail.com>
2018-06-11 09:44:08 -0400

    add comment

--- routes/userController.js
+++ routes/userController.js
@@ -8,6 +8,7 @@ router.get('/', (req, res, next) => {
 
   // Find all users
   user.find()
+  // then render out all data that match the user model.
     .then((listAllusers) => {
 
       res.render('user/index', { listAllusers: listAllusers })
